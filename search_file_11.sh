1973881a11e2e7c02b761811e6415da9abdd63cf
alex1436183 <alex1436183@gmail.com>
2024-11-15 14:00:38 +0200

    Update search_file_11.sh
    fix

--- search_file_11.sh
+++ search_file_11.sh
@@ -2,12 +2,12 @@
 out_file="$1"
 ext="$2"
 dir="$3"
-find "$dir" -name "*$ext" > "$out_file"
 
 if [[ ! -d "$dir" ]]; then
     echo " Directory: "$dir" unavailable or does not exist " 
     exit 1
 fi
 
+find "$dir" -name "*$ext" > "$out_file"
 echo "File names with specified extensions have been move to the file $out_file"
 
