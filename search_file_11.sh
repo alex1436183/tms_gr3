f2caa3f20ba89a79aa6c38ff30968855ec947fad
mazurink <mazurink@mail.ru>
2024-11-15 14:13:24 +0300

    add checking for quantity of input parameters

--- search_file_11.sh
+++ search_file_11.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#check for number of input parameters
+if [ "$#" -ne 3 ]
+then
+  echo "Incorrect number of arguments"
+  exit 1
+fi
 out_file="$1"
-ext="$2"
+extension="$2"
 dir="$3"
-find "$dir" -name "*$ext" > "$out_file"
+find "$dir" -name "*$extension" > "$out_file"
 echo "Имена файлов с заданными расширениями перемещены в файл $out_file"
  
