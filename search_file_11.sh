3bd7836cc1c727afc325527f165eb20cc87f3d49
alex1436183 <alex1436183@gmail.com>
2024-11-15 11:48:42 +0000

    added new feature

--- search_file_11.sh
+++ search_file_11.sh
@@ -3,5 +3,11 @@ out_file="$1"
 ext="$2"
 dir="$3"
 find "$dir" -name "*$ext" > "$out_file"
-echo "Имена файлов с заданными расширениями перемещены в файл $out_file"
+
+if [[ ! -d "$dir" ]]; then
+    echo " Directory: "$dir" unavailable or does not exist " 
+    exit 1
+fi
+
+echo "File names with specified extensions have been move to the file $out_file"
 
